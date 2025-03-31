c33ceab849916048ace901cb1dd41fff3330aa48
binuraowin <binura.owin1@gmail.com>
2025-03-31 21:13:55 +0530

    from memory

--- main.go
+++ main.go
@@ -1,16 +1,82 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
 func main() {
-	myname := "John Doe"
+	myname := "John Doess"
 	app := fiber.New()
-	// This is a simple Go program that prints "Hello, World!" to the console.
 	println(myname)
+
+	todos := []Todo{}
+
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+
+		var x int = 5
+		var p *int = &x
+
+		println("Value of x:", x)
+		println("Address of x:", &x)
+		println("Value of p:", p)
+		println("Value pointed to by p:", *p)
+		println("Address of p:", &p)
+		return c.Status(200).JSON(todos)
+	},
+	)
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{
+				"message": "Todo body is required",
+			})
+		}
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+		return c.Status(201).JSON(todo)
+	})
+
+	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, todo := range todos {
+			if fmt.Sprint(todo.ID) == id {
+				todo.Completed = !todo.Completed
+				todos[i] = todo
+				return c.Status(200).JSON(todo)
+			}
+		}
+		return c.Status(404).JSON(fiber.Map{
+			"message": "Todo not found",
+		})
+	})
+	app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		for i, todo := range todos {
+			if fmt.Sprint(todo.ID) == id {
+				todos = append(todos[:i], todos[i+1:]...)
+				return c.Status(200).JSON(fiber.Map{
+					"message": "Todo deleted",
+				})
+			}
+		}
+		return c.Status(404).JSON(fiber.Map{
+			"message": "Todo not found",
+		})
+	})
 	log.Fatal(app.Listen(":3000"))
-	// The program uses the built-in println function to output the string.
 }
